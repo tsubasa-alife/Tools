d0bd9bfb83ef7c389d21160c9a160f36ac778eb9
tsubasa-psychiatry <tsubasa.robotics@gmail.com>
2022-12-31 19:25:06 +0900

    シングルトン実装用のコードを追加

---
+++ Unity/Extentions/SingletonMonoBehaviour.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+	private static T instance;
+ 
+	public static T Instance
+	{
+		get
+		{
+			if (instance == null)
+			{
+				instance = (T)FindObjectOfType(typeof(T));
+ 
+				if (instance == null)
+				{
+					Debug.LogError(typeof(T) + " is not Found");
+				}
+			}
+ 
+			return instance;
+		}
+	}
+
+	private void Awake()
+	{
+		if (this != Instance)
+		{
+			Destroy(this.gameObject);
+			return;
+		}
+
+		// シーンを跨いでも消えないように設定
+		DontDestroyOnLoad(this.gameObject);
+	}
+}
