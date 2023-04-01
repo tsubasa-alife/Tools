4989a0bcf2cf20186e145a4324397afb72b7ee31
tsubasa-alife <tsubasa.robotics@gmail.com>
2023-04-02 03:37:09 +0900

    カスタムボタン修正

--- Unity/CustomUI/CustomButton.cs
+++ Unity/CustomUI/CustomButton.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
@@ -9,77 +10,9 @@ using TMPro;
 [RequireComponent(typeof(Image))]
 public abstract class CustomButton : MonoBehaviour,IPointerClickHandler,IPointerDownHandler,IPointerUpHandler
 {
-	public enum ButtonType
-	{
-		Default
-	}
-
-	public ButtonType buttonType;
-	[SerializeField] private bool isActive = true;
-	private Image image = null;
-	private TextMeshProGUI buttonText = null;
-	public System.Action onClickEvent;
-
-	public bool IsActive
-	{
-		get
-		{
-			return isActive;
-		}
-		set
-		{
-			isActive = value;
-			Activate();
-		}
-	}
-
-	public Image Image
-	{
-		get
-		{
-			return image;
-		}
-	}
-
-	public TextMeshProGUI ButtonText
-	{
-		get
-		{
-			return buttonText;
-		}
-	}
-
-	private void Awake()
-	{
-		image = this.gameObject.GetComponent<Image>();
-		buttonText = this.gameObject.GetComponentInChildren<TextMeshProUGUI>();
-	}
-
-	private void Activate()
-	{
-		if(IsActive)
-		{
-			if(Image != null)
-			{
-				Image.color = new Color(Image.color.r,Image.color.g,Image.color.b,1);
-			}
-			if(ButtonText != null)
-			{
-				ButtonText.color = new Color(ButtonText.color.r,ButtonText.color.g,ButtonText.color.b,1);
-			}
-		}
-		else
-		{
-			if(Image != null)
-			{
-				Image.color = new Color(Image.color.r,Image.color.g,Image.color.b,0.5f);
-			}
-			if(ButtonText != null)
-			{
-				ButtonText.color = new Color(ButtonText.color.r,ButtonText.color.g,ButtonText.color.b,0.5f);
-			}
-		}
-	}
+	[SerializeField] private Image image;
+	[SerializeField] private TextMeshProGUI buttonText;
+	public Action onClickEvent;
 
 	// クリック or タップした時の挙動は必ず実装する
 	public abstract void OnPointerClick(PointerEventData eventData);
