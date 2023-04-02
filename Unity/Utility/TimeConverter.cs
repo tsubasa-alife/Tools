31db956d0cb087a3f888d73fb247500c1f7added
tsubasa-alife <tsubasa.robotics@gmail.com>
2023-04-02 10:32:27 +0900

    秒数を変換するメソッドを追加

--- Unity/Utility/TimeConverter.cs
+++ Unity/Utility/TimeConverter.cs
@@ -9,6 +9,14 @@ public class TimeConverter
 		float seconds = timeSeconds - minutes * 60;
 		return minutes.ToString("00") + ":" + ((int)seconds).ToString("00");
 	}
+
+	public static string SecondsToHours(float timeSeconds)
+	{
+		int hours = (int)timeSeconds / 3600;
+		int minutes = (int)(timeSeconds - hours * 60) / 60;
+		float seconds = timeSeconds - hours * 3600 - minutes * 60;
+		return hours.ToString("00") + ":" + minutes.ToString("00") + ":" + ((int)seconds).ToString("00");
+	}
 }
 
 
