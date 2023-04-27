3cc21b6955eae508cb1519e5aae8a59a2870136a
tsubasa-alife <tsubasa.robotics@gmail.com>
2023-04-27 16:54:35 +0900

    TimeConverter修正

--- Unity/Utility/TimeConverter.cs
+++ Unity/Utility/TimeConverter.cs
@@ -13,7 +13,7 @@ public class TimeConverter
 	public static string SecondsToHours(float timeSeconds)
 	{
 		int hours = (int)timeSeconds / 3600;
-		int minutes = (int)(timeSeconds - hours * 60) / 60;
+		int minutes = (int)(timeSeconds - hours * 3600) / 60;
 		float seconds = timeSeconds - hours * 3600 - minutes * 60;
 		return hours.ToString("00") + ":" + minutes.ToString("00") + ":" + ((int)seconds).ToString("00");
 	}
