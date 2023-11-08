782d814339a2e77e9e80c44962b90a8602bed967
tsubasa-alife <tsubasa.robotics@gmail.com>
2023-11-09 01:33:24 +0900

    ビルドメソッド更新

--- Unity/Editor/ProjectBuilder.cs
+++ Unity/Editor/ProjectBuilder.cs
@@ -10,34 +10,35 @@ using UnityEditor.Build.Reporting;
 
 public class ProjectBuilder
 {
-    [MenuItem("Tools/Build/CustomBuild")]
-    public static void CustomBuild()
-    {
-        Build();
-    }
+	[MenuItem("Tools/Build/CustomBuild")]
+	public static void CustomBuild()
+	{
+		Build();
+	}
 
-    private static void Build()
-    {
-        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-        
-        // ビルド出力先
-        buildPlayerOptions.locationPathName = "Build/hogehoge.apk";
-        // 開発ビルドかどうか
-        buildPlayerOptions.options = BuildOptions.Development;
-        // ビルドするプラットフォームの指定
-        buildPlayerOptions.target = BuildTarget.Android;
+	private static void Build()
+	{
+		BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+		// ビルドシーンの指定
+		buildPlayerOptions.scenes = EditorBuildSettingsScene.GetActiveSceneList(EditorBuildSettings.scenes);
+		// ビルド出力先
+		buildPlayerOptions.locationPathName = "Build/hogehoge.apk";
+		// 開発ビルドかどうか
+		buildPlayerOptions.options = BuildOptions.Development;
+		// ビルドするプラットフォームの指定
+		buildPlayerOptions.target = BuildTarget.Android;
 
-        // ビルドの実行と結果通知
-        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
-        BuildSummary summary = report.summary;
+		// ビルドの実行と結果通知
+		BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+		BuildSummary summary = report.summary;
 
-        if (summary.result == BuildResult.Succeeded)
-        {
-            Debug.Log("Build succeeded");
-        }
-        else if (summary.result == BuildResult.Failed)
-        {
-            Debug.LogError("Build Failed");
-        }
-    }
+		if (summary.result == BuildResult.Succeeded)
+		{
+			Debug.Log("Build succeeded");
+		}
+		else if (summary.result == BuildResult.Failed)
+		{
+			Debug.LogError("Build Failed");
+		}
+	}
 }
